e4810ac635b2ba92c1cc1ddba85a5b576df509d7
Fameho <pavelcherentsov23@yandex.ru>
2025-12-25 14:57:48 +0500

    +tg bot

--- js/script.js
+++ js/script.js
@@ -1,5 +1,32 @@
-document.getElementById('contactForm').addEventListener('submit', function(e) {
+const TOKEN = "8254708263:AAG44ieJsBA-R1QzYMmvVg_2ytPFsn8bBvA";
+const CHAT_ID = "691818395";
+
+document.getElementById("contactForm").addEventListener("submit", function(e) {
   e.preventDefault();
-  alert('Заявка отправлена! Мы свяжемся с вами.');
-  this.reset();
+
+  const name = this.querySelector('input[placeholder="Имя"]').value;
+  const phone = this.querySelector('input[placeholder="Телефон"]').value;
+  const message = this.querySelector('textarea').value;
+
+  const text =
+    `📩 Новая заявка\n\n` +
+    `👤 Имя: ${name}\n` +
+    `📞 Телефон: ${phone}\n` +
+    `💬 Комментарий: ${message || "-"}`;
+
+  fetch(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      chat_id: CHAT_ID,
+      text: text
+    })
+  })
+  .then(() => {
+    alert("Заявка отправлена!");
+    this.reset();
+  })
+  .catch(() => {
+    alert("Ошибка отправки");
+  });
 });
